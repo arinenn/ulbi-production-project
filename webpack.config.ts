e35855cae5088fefce23f18cf568cdba4be8be3d
arinenn <arinenn@ya.ru>
2023-02-04 12:34:27 +0700

    fixed wrong build path

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
 }
 
